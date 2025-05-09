10eb1f91b7fb57b9c4aeedf70028842703af1cfe
dawc17 <dawid.czaplicki2008@gmail.com>
2025-05-09 11:30:05 +0200

    lazyvim!

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,25 +1,2 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-
-local opts = { noremap = true }
-
-vim.keymap.set('n', '<C-h>', '<C-W>h', opts)
-vim.keymap.set('n', '<C-j>', '<C-W>j', opts)
-vim.keymap.set('n', '<C-k>', '<C-W>k', opts)
-vim.keymap.set('n', '<C-l>', '<C-W>l', opts)
-
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-
-vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal left<CR>")
-vim.keymap.set("n", "<C-n>", ":Neotree filesystem close<CR>")
-vim.keymap.set("n", "<leader>cc", ":CopilotChatToggle<CR>")
-
+-- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
----@diagnostic disable-next-line: different-requires
-require("lazy").setup("plugins")
-require("catppuccin").setup()
-vim.cmd.colorscheme "redish"
--- .config/nvim/lua/plugins/lualine-wal.lua
+++
@@ -1,3 +0,0 @@
-return {
-  "nativerv/lualine-wal.nvim"
-}
